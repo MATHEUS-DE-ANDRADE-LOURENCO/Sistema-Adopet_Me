a7ca37dc2c6f3c085b5807ef27ed1c005543feee
Murillo Cardoso Ferreira <murillocardosoferreira@gmail.com>
2025-10-09 17:28:15 -0300

    Pequena alteração na HomePage, a tela estava dimensionada incorretamente

--- adopetme-frontend/src/pages/HomePage.tsx
+++ adopetme-frontend/src/pages/HomePage.tsx
@@ -6,14 +6,24 @@ import HowItWorksSection from '../components/HowItWorks';
 
 const HomePage: React.FC = () => {
   return (
-    <div className="min-h-screen bg-stone-50 antialiased font-sans">
+    <div className="w-screen flex flex-col overflow-x-hidden">
+      {/* Navbar */}
       <Navbar />
-      <main>
+
+      {/* Conteúdo principal */}
+      <main className="flex justify-center flex-col">
         <HeroSection />
         <HowItWorksSection />
+        {/* Você pode adicionar outras seções aqui */}
       </main>
+
+      {/* Footer */}
+      <footer className="w-full bg-yellow-600 text-white py-6 text-center">
+        &copy; {new Date().getFullYear()} adopet.me - Todos os direitos reservados
+      </footer>
     </div>
   );
 };
+
 
 export default HomePage;
