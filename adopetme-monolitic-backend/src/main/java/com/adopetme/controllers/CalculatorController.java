245cf18fd87c0ccbbdd2d449114f772160aa5d63
Danilo Honda <hondadanilo6@gmail.com>
2025-10-14 16:24:23 +0000

    remove arquivos teste matheus

--- adopetme-monolitic-backend/src/main/java/com/adopetme/controllers/CalculatorController.java
+++ adopetme-monolitic-backend/src/main/java/com/adopetme/controllers/CalculatorController.java
@@ -1,27 +1,12 @@
-package com.adopetme.controllers;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
-import com.adopetme.services.Calculator;
 
 @RestController
 @RequestMapping("/calculator")
 public class CalculatorController {
-    @Autowired
-    private Calculator calculatorService;
-
     @GetMapping("/soma/{numberOne}/{numberTwo}")
-    public double soma(@PathVariable double numberOne, @PathVariable double numberTwo) {
-        return calculatorService.somar(numberOne, numberTwo);
+    public double soma(@PathVariable int numberOne, @PathVariable int numberTwo) {
+        return numberOne + numberTwo;
     }
-
-    @GetMapping("/subtracao/{numberOne}/{numberTwo}")
-    public double subtracao(@PathVariable double numberOne, @PathVariable double numberTwo) {
-        return calculatorService.subtrair(numberOne, numberTwo);
-    }
-
 }
-
