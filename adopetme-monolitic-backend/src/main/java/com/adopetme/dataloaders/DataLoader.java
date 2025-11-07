73e1472ce8646ea55e8d7d0366259d6f4aa7d7fb
DaniloHonda <hondadanilo6@gmail.com>
2025-11-07 17:59:03 -0300

    fix/backend bug

--- adopetme-monolitic-backend/src/main/java/com/adopetme/dataloaders/DataLoader.java
+++ adopetme-monolitic-backend/src/main/java/com/adopetme/dataloaders/DataLoader.java
@@ -4,27 +4,30 @@ import com.adopetme.models.User;
 import com.adopetme.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; 
 
 @Configuration
 public class DataLoader implements CommandLineRunner {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    private final BCryptPasswordEncoder bCryptPasswordEncoder; 
 
-    public DataLoader(UserRepository userRepository) {
+    public DataLoader(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
     public void run(String... args) {
+
         if (userRepository.findByEmail("admin@adopetme.com").isEmpty()) {
             User admin = new User();
             admin.setEmail("admin@adopetme.com");
+            admin.setLogin("admin"); 
+            admin.setNome("Admin");   
             admin.setSenha(bCryptPasswordEncoder.encode("admin123"));
             admin.setTipoUsuario("ADMIN");
             userRepository.save(admin);
         }
-
     }
 }
